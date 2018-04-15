e6715fe0abeaf2abc1711d0fabdc72a349011a97
daniel <daniel@mester.se>
2018-04-15 15:53:42 +0200

    Cleanup.

--- runtests.bat
+++
@@ -1,2 +0,0 @@
-set PATH=%PATH%;C:\php;C:\php\ext
-./vendor/bin/phpunit --bootstrap vendor/autoload.php tests/EmailTest
--- start.bat
+++
@@ -1,5 +0,0 @@
-set PATH=%PATH%;C:\php;C:\php\ext
-set MAGICK_HOME=C:\php\imagemagick
-start php -S localhost:8000
-ping 127.0.0.1>NUL
-start http://localhost:8000/

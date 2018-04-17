1b7e480075319e896d03ae4d355d8272ee53fd3a
daniel <daniel@mester.se>
2018-04-17 18:12:06 +0200

    Added copyright banners.

--- src/Canvas/Rasterization/EdgeSuperSampleIntersection.php
+++ src/Canvas/Rasterization/EdgeSuperSampleIntersection.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * This file is part of Jdenticon for PHP.
+ * https://github.com/dmester/jdenticon-php/
+ * 
+ * Copyright (c) 2018 Daniel Mester Pirttijärvi
+ * 
+ * For full license information, please see the LICENSE file that was 
+ * distributed with this source code.
+ */
+
 namespace Jdenticon\Canvas\Rasterization;
 
 class EdgeSuperSampleIntersection
